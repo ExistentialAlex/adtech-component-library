c1d38081cb2acf716fc2a0bf201075fdba256b0f
Alexander Hewitson <alexander.hewitson@sky.uk>
2023-01-23 17:11:00 +0000

    feat(templates): add template files

---
+++ scripts/components/templates/component-template.stories.js
@@ -0,0 +1,26 @@
+import ComponentTemplate from './component-template.vue';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Example/ComponentTemplate',
+  component: ComponentTemplate,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {},
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { ComponentTemplate },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<ComponentTemplate v-bind="args" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  myprop: 'My test!',
+};
---
+++ scripts/components/templates/component-template.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  myprop: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <h1 class="my-class">{{ myprop }}</h1>
+</template>
+
+<style scoped>
+.my-class {
+  color: #fff;
+}
+</style>
