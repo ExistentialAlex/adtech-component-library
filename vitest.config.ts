f8214bf01fbb95ea9ba09382a13978b81d6d2139
Alexander Hewitson <alexander.hewitson@sky.uk>
2023-01-24 10:50:01 +0000

    feat(): update configs to allow vue testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { configDefaults, defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     exclude: [...configDefaults.exclude, './scripts/**'],
   },
