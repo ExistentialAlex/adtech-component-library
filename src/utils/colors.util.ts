59a720520becb7942a74f0c6ddaa440ea3bb62ee
Alexander Hewitson <alexander.hewitson@sky.uk>
2023-01-27 10:23:40 +0000

    feat(colors.util): add methods to get classes

--- src/utils/colors.util.ts
+++ src/utils/colors.util.ts
@@ -1,11 +1,7 @@
-import {
-  AdTechComponentColors,
-  AdTechNumericColors,
-  AdTechStatusColors,
-} from '../enums';
+import { AdTechComponentColors, AdTechNumericColors, AdTechStatusColors } from '../enums';
 
 /**
- * 
+ *
  * @returns An array of all the possible component colors
  */
 export const getAllColorsAsArray = () => {
@@ -14,4 +10,59 @@ export const getAllColorsAsArray = () => {
     ...Object.values(AdTechStatusColors),
     ...Object.values(AdTechNumericColors),
   ];
+};
+
+export const getColorBgClasses = (
+  color?: AdTechComponentColors | AdTechNumericColors | AdTechStatusColors,
+  hover?: boolean
+): string[] => {
+  const classes: string[] = [];
+  const btnClr = color || AdTechComponentColors.primary;
+
+  switch (btnClr) {
+    case AdTechComponentColors.primary:
+    case AdTechComponentColors.secondary:
+    case AdTechComponentColors.accent:
+    case AdTechNumericColors.positive:
+    case AdTechNumericColors.negative: {
+      classes.push(`${btnClr}-bg`);
+      if (hover) {
+        classes.push(`${btnClr}-bg-hover`);
+      }
+      break;
+    }
+    case AdTechStatusColors.error:
+    case AdTechStatusColors.success:
+    case AdTechStatusColors.warning:
+    case AdTechStatusColors.info:
+    case AdTechStatusColors.neutral: {
+      classes.push(`${btnClr}-fill`);
+      if (hover) {
+        classes.push(`${btnClr}-fill-hover`);
+      }
+      break;
+    }
+  }
+
+  return classes;
+};
+
+export const getBrClasses = ({
+  rounded,
+  sharp,
+}: {
+  rounded: boolean;
+  sharp: boolean;
+}): string[] => {
+  const classes = [];
+
+  if (rounded) {
+    classes.push('br-rounded');
+  }
+
+  if (sharp) {
+    classes.push('br-sharp');
+  }
+
+  return classes;
 };
