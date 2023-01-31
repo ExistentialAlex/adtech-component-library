feab5d7799ff920809f149b70a1cac3c3602f4ac
Alexander Hewitson <alexander.hewitson@sky.uk>
2023-01-31 15:02:58 +0000

    chore(): remove old hello-world component

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import type { App } from 'vue';
 import './style.scss';
-import { AdtMenu, AdtTextField, AdtButton, HelloWorld } from './components';
+import { AdtMenu, AdtTextField, AdtButton } from './components';
 
 export default {
   install: (app: App) => {
     app.component('AdtMenu', AdtMenu);
     app.component('AdtTextField', AdtTextField);
     app.component('AdtButton', AdtButton);
-    app.component('HelloWorld', HelloWorld);
   },
 };
 
-export { AdtMenu, AdtTextField, AdtButton, HelloWorld };
+export { AdtMenu, AdtTextField, AdtButton };
